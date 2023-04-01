e5026db392c35c77f755196c29c4c39ea2bc35f5
Adrian <pogorzelski.adrian@gmail.com>
2023-04-01 10:36:57 +0200

    Initial commit

---
+++ src/main/java/wsb/wsb_junit/WsbJUnitApplication.java
@@ -0,0 +1,13 @@
+package wsb.wsb_junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbJUnitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbJUnitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/wsb_junit/WsbJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.wsb_junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbJUnitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
